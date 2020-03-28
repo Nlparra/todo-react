42dbacd90edbf62a3a96e2fa4c288c1aeb7cdef1
Nelson <nlparra.dev@gmail.com>
2020-03-28 19:12:07 +0000

    working on the code for function of the list

---
+++ src/js/component/List.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+export default function List() {
+	const [list, setList] = useState([]);
+
+	const handleEnter = e => {
+		console.log(e.target.value);
+
+		const { key, target } = e;
+		if (key === "Enter") {
+			setList(list => [...list, target.value]);
+			// target.value = "";
+		}
+	};
+
+	console.log(list);
+
+	return (
+		<ul className="list-group">
+			<li className="input-group-text" id="inputGroup-sizing-sm">
+				<input
+					type="text"
+					className="form-control"
+					aria-label="Sizing example input"
+					aria-describedby="inputGroup-sizing-sm"
+					onKeyDown={handleEnter}
+				/>
+			</li>
+		</ul>
+	);
+}
